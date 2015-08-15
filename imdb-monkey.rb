f82f52c94a68788179999774358dbc9addd927a5
Suyesh Bhandari <suyeshb@gmail.com>
2015-08-14 23:07:58 -0400

    testers

--- imdb-monkey.rb
+++ imdb-monkey.rb
@@ -3,19 +3,18 @@ This Script finds the Average rating and highest/lowest rated movie of an actor'
 =end
 require_relative 'imdb_movie'
 require_relative 'imdb_actor'
-=begin
+
 base_url = "http://www.imdb.com/"
 #movie_url = "http://www.imdb.com/title/#{movie_code}/"
 #award_page = "http://www.imdb.com/name/#{actor_code}/awards?"
-puts "************************************************************************"
-puts "Right Now i can only search Actors and their movies with its release year :(".rjust(20)
-puts "Pretty soon i will be able to do much much more.".rjust(20)
-puts "For now just type the name of the actor you want to search.".rjust(20)
-puts "************************************************************************"
+
 print "Type a Actor's name: "
 user_input = gets.chomp
 
 imdb = ImdbActor.new(user_input)
 
-puts imdb.actor_lowest_rated_movie
-=end
+imdb.actor_movies.each {|movie, year| puts "#{movie}.....#{year}"}
+puts "#{user_input}'s upcoming #{imdb.actor_upcoming_movies.length > 1 ? "movies are " : "movie is "} #{imdb.actor_upcoming_movies}"
+puts "Highest rated movie : #{imdb.actor_highest_rated_movie}"
+puts "Lowest rated movie : #{imdb.actor_lowest_rated_movie}"
+puts "#{user_input}'s Average movie rating based on all movies is #{imdb.actor_average_movie_rating}"
