41ed4e2b2789b2d5f0a349c2f7003667a9e0f500
Suyesh Bhandari <suyeshb@gmail.com>
2015-09-08 19:35:54 -0400

    Readme mod

--- imdb-monkey.rb
+++ imdb-monkey.rb
@@ -7,7 +7,7 @@ require_relative 'imdb_actor'
 base_url = "http://www.imdb.com/"
 #movie_url = "http://www.imdb.com/title/#{movie_code}/"
 #award_page = "http://www.imdb.com/name/#{actor_code}/awards?"
-
+puts "\n"
 print "Type a Actor's name: "
 user_input = gets.chomp
 puts "\n"
